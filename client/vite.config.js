ca0e4966f812ff0205c0131c262f991c6850455f
Ritesh Maurya <122164877+Riteshmaurya07@users.noreply.github.com>
2026-03-03 12:54:50 +0530

    Update Vite config to use environment variables
    Refactor Vite configuration to load environment variables for backend URL.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.BACKEND_URI || 'http://localhost:5000',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        }
       }
     }
   }
