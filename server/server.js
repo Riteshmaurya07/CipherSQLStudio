9ac3ba086048e219095c370efbd75ccf0b877926
Ritesh Maurya <122164877+Riteshmaurya07@users.noreply.github.com>
2026-03-11 01:03:26 +0530

    Update CORS middleware to restrict origins

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    process.env.FRONTEND_URI
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
